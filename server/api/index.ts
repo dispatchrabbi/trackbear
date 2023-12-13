6769f783d67c52017496f5d1c60e1d6355b3ae7b
Eli Morris-Heft <eli.morris.heft@gmail.com>
2023-12-13 10:03:22 -0500

    Start real project API endpoints

--- server/api/index.ts
+++ server/api/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import pingRouter from './ping.ts';
 import authRouter from './auth.ts';
 import userRouter from './user.ts';
+import projectsRouter from './projects.ts';
 
 const apiRouter = Router();
 
 apiRouter.use('/ping', pingRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/user', userRouter);
+apiRouter.use('/projects', projectsRouter);
 
 export default apiRouter;
--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,57 +1,80 @@
-import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+
+import { requireUser } from '../lib/auth.ts';
+import dbClient from "../lib/db.ts";
+import { PROJECT_STATE } from '../lib/states.ts';
+
+import { Project } from "@prisma/client";
+import { RequestWithUser } from '../lib/auth.ts';
 
 const projectsRouter = Router();
 
 // GET /projects - return all projects for the user
-projectsRouter.get('/', async (req, res) => {
-  const db = req.app.get('db') as PrismaClient;
+projectsRouter.get('/', requireUser, async (req, res, next) => {
+  let projects: Project[];
+  try {
+    projects = await dbClient.project.findMany({
+      where: {
+        ownerId: (req as RequestWithUser<typeof req>).user.id,
+        state: PROJECT_STATE.ACTIVE
+      }
+    });
+  } catch(err) { return next(err); }
 
-  const projects = await db.project.findMany({
-    where: { state: 'active '},
-    select: { id: true, uuid: true, title: true, type: true, state: true, starred: true }
-  });
-  res.status(200).json(projects);
+  res.status(200).send(projects);
 });
 
 // GET /projects/:id - return a specific project
-projectsRouter.get('/:id', async (req, res) => {
-  const db = req.app.get('db') as PrismaClient;
+projectsRouter.get('/:id', requireUser, async (req, res, next) => {
+  // TODO: validate :id
+  let project: Project | null;
+  try {
+    project = await dbClient.project.findUnique({
+      where: {
+        id: +req.params.id,
+        ownerId: (req as RequestWithUser<typeof req>).user.id,
+        state: PROJECT_STATE.ACTIVE
+      }
+    });
+  } catch(err) { return next(err); }
 
-  const project = await db.project.findUnique({
-    where: { id: +req.params.id },
-  });
-  res.status(200).json(project);
+  if(project) {
+    res.status(200).send(project);
+  } else {
+    res.status(404).send();
+  }
 });
 
-// PUT /projects - create a new project
-projectsRouter.put('/', (req, res) => {
+// // PUT /projects - create a new project
+// projectsRouter.put('/', (req, res) => {
 
-});
+// });
 
-// POST /projects:id - modify the given project
-projectsRouter.post('/:id', async (req, res) => {
-  const db = req.app.get('db') as PrismaClient;
+// // POST /projects:id - modify the given project
+// projectsRouter.post('/:id', async (req, res) => {
+//   const db = req.app.get('db') as PrismaClient;
 
-  // this is, for now, terribly unsafe!
-  // I'll need to do some validation on this before actually deploying it
-  const project = await db.project.update({
-    where: { id: +req.params.id },
-    data: req.body,
-  });
+//   // this is, for now, terribly unsafe!
+//   // I'll need to do some validation on this before actually deploying it
+//   const project = await db.project.update({
+//     where: { id: +req.params.id },
+//     data: req.body,
+//   });
 
-  res.json(project);
-});
+//   res.json(project);
+// });
 
-// DELETE /projects/:id - archive a project
-projectsRouter.delete('/:id', async (req, res) => {
-  const db = req.app.get('db') as PrismaClient;
+// // DELETE /projects/:id - archive a project
+// projectsRouter.delete('/:id', async (req, res) => {
+//   const db = req.app.get('db') as PrismaClient;
 
-  const project = await db.project.update({
-    where: { id: +req.params.id },
-    data: { state: 'archived' },
-    select: { id: true, state: true },
-  });
+//   const project = await db.project.update({
+//     where: { id: +req.params.id },
+//     data: { state: 'archived' },
+//     select: { id: true, state: true },
+//   });
 
-  res.status(200).json({ id: project.id, state: project.state });
-});
+//   res.status(200).json({ id: project.id, state: project.state });
+// });
+
+export default projectsRouter;
