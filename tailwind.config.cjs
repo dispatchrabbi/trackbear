8d91397405c88bf2b533a9f9c3f146bd26e601f8
Eli Morris-Heft <eli.morris.heft@gmail.com>
2023-12-18 17:31:52 -0500

    Fix tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,ts,vue}',
+    './client/src/**/*.{html,js,ts,vue}',
   ],
 	theme: {
 		extend: {
