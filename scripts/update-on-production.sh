d859f8bf8ec53812a2065ea7f9dcef0f21b183e8
Eli Morris-Heft <eli.morris.heft@gmail.com>
2023-12-21 09:41:07 -0500

    Update the production update script

--- scripts/update-on-production.sh
+++ scripts/update-on-production.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
+echo "Switching Node versions..."
+nvm use
+
+echo "Running migrations..."
+npx prisma migrate deploy
+
 echo "Building the front end..."
+npx prisma generate
 npm run client:build
 
 echo "Restarting the app..."
-pm2 restart ecosystem.config.cjs
+pm2 restart ./ecosystem.config.cjs
