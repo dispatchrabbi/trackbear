6375c784ab19ad49241178d4d1e3cb0e2f1dbed2
Eli Morris-Heft <eli.morris.heft@gmail.com>
2023-12-20 22:11:22 -0500

    Put the build files in the right directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ vue(), visualizer() ],
-  publicDir: '../public',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
   },
 });
