c72d330907b6a6ae986011fd2bb308b740e78a12
Eli Morris-Heft <eli.morris.heft@gmail.com>
2024-09-24 20:30:20 -0400

    Implement instrumented traces for development along with dev jaeger server

---
+++ instrumentation.ts
@@ -0,0 +1,20 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+        url: 'http://localhost:4318/v1/metrics',
+    }),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
