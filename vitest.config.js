d1869765fa39021b62366c22b43c23707f29d331
Eli Morris-Heft <eli.morris.heft@gmail.com>
2024-10-13 21:30:39 -0400

    Enable coverage in vitest reporting

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 export default defineConfig({
  test: {
    coverage: {
+    enabled: true,
     // TODO: slowly remove these as coverage expands
      exclude: [
       ...coverageConfigDefaults.exclude,
