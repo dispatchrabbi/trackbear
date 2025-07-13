cc64c1d5492ed96753464d277dcc90a496a99141
Eli Morris-Heft <eli.morris.heft@gmail.com>
2025-07-12 22:00:31 -0400

    Update Prisma

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -1,6 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
-const dbClient = new PrismaClient();
+import { getNormalizedEnv } from './env';
+
+const env = await getNormalizedEnv();
+const connectionString = `postgresql://${env.DATABASE_USER}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}/${env.DATABASE_NAME}`;
+
+const adapter = new PrismaPg({ connectionString });
+const dbClient = new PrismaClient({ adapter });
 
 export default dbClient;
 
