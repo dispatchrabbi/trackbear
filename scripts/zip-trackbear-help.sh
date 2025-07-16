47417e5dc8ea1e59c315e8452479db35597eb8e9
Eli Morris-Heft <eli.morris.heft@gmail.com>
2025-07-15 21:16:29 -0400

    Move zip function to a script and rename zip file

---
+++ scripts/zip-trackbear-help.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This whole script exists because all the options for tar are different on different systems.
+# We could have had it all...
+echo "Starting trackbear-help zip:"
+
+# cd into the .vitepress directory (because not all tars let you strip leading path components)
+echo "Moving into .vitepress directory..."
+cd ./help-docs/.vitepress
+# copy the built help docs over to a better-named directory (because not all tars let you s/// filenames)
+echo "Copying dist to help-docs..."
+cp -r ./dist ./trackbear-help
+# make the archive (this one ought to be pretty standard)
+echo "Creating archive at trackbear-help.tar.gz..."
+tar -czf ./trackbear-help.tar.gz ./trackbear-help/**/*.*
+# remove the copy
+echo "Removing trackbear-help copy..."
+rm -rf ./trackbear-help
+# get back out to where we were supposed to be
+echo "Getting the heck out of Dodge..."
+cd -
+
+echo "trackbear-help zip complete!"
