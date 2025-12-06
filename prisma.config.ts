9465ec4f16c25731a5f7d6d81371bbea9a607b16
Eli Morris-Heft <eli.morris.heft@gmail.com>
2025-12-05 21:56:12 -0500

    Update Prisma to v7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,28 +1,26 @@
 import path from 'node:path';
+import process from 'node:process';
+
 import dotenv from 'dotenv';
 import type { PrismaConfig } from 'prisma';
-// import { initDbClient, getDbAdapter } from './server/lib/db.ts';
+
+import { makeConnectionString, validateConnectionParams } from './server/lib/db.ts';
 
 dotenv.config();
+const datasourceUrl = makeConnectionString(validateConnectionParams({
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  name: process.env.DATABASE_NAME,
+  schema: process.env.DATABASE_SCHEMA,
+}));
 
 export default {
-  // experimental: {
-  //   adapter: true,
-  // },
   schema: path.join('prisma', 'schema.prisma'),
   migrations: {
     path: path.join('prisma', 'migrations'),
   },
-  // not supported for CLI yet
-  // adapter: async () => {
-  //   initDbClient(
-  //     process.env.DATABASE_USER!,
-  //     process.env.DATABASE_PASSWORD!,
-  //     process.env.DATABASE_HOST!,
-  //     process.env.DATABASE_NAME!,
-  //     process.env.DATABASE_SCHEMA ?? undefined,
-  //   );
-
-  //   return getDbAdapter();
-  // },
+  datasource: {
+    url: datasourceUrl,
+  },
 } satisfies PrismaConfig;
